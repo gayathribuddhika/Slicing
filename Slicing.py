24d1ea972fcdc9faa57f63a75ed473f8e7f0d5b9
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-02-22 23:41:06 +0530

    Sicing
    Slicing in Python

---
+++ Slicing.py
@@ -0,0 +1,16 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num = [1,2,3,4,5,6,7,8,9]
+>>> num
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> num[2:5]
+[3, 4, 5]
+>>> num[0:9:2]
+[1, 3, 5, 7, 9]
+>>> num[1:8:2]
+[2, 4, 6, 8]
+>>> num[::-2]
+[9, 7, 5, 3, 1]
+>>> num[-1:-9:-3]
+[9, 6, 3]
+>>> 
